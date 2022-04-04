d640f6e81cef7cff33f63a2495aa04f64db901d0
Dima <dima@MacBook-Pro-14.local>
2022-04-04 10:22:35 +0300

    Initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+INSTALL_PATH=/app
+SERVER_PID=$INSTALL_PATH/tmp/pids/server.pid
+
+if [ -f $SERVER_PID ]; then
+    rm -f $SERVER_PID
+fi
+
+# Installing rails is /app dir is empty. 
+# Usefull when using volumes
+if [ -f "$INSTALL_PATH/Gemfile" ]; then
+    bundle    install
+else
+    rails new $INSTALL_PATH -j esbuild --css tailwind --database=mysql --no-sprockets
+fi
+
+exec "$@"
